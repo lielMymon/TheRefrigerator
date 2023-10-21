a3c8d8b03ee6c024096ff3625b4c780126df4768
lielMymon <110356660+lielMymon@users.noreply.github.com>
2023-10-22 01:06:41 +0300

    Added Project

---
+++ TheRefrigerator/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace TheRefrigerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //הגדרת מקרר עם 2 מדפים כל מדף מכיל פריטים
+            DateTime d = new DateTime(2024, 02, 02);
+            DateTime d2 = new DateTime(2002, 02, 02);
+            DateTime d3 = new DateTime(2023, 10, 20);
+            DateTime d4 = new DateTime(2011, 04, 03);
+            Item x1 = new Item("cheese1", 3, eType.food, eCosher.milk, d, 21.7);
+            Item x2 = new Item("cheese2", 3, eType.food, eCosher.milk, d2, 4.3);
+            Shelf y = new Shelf(2, 25);
+            y.ItemsList.Add(x1);
+            y.ItemsList.Add(x2);
+
+
+            Item x3 = new Item("cheese3", 3, eType.food, eCosher.milk, d3, 10);
+            Item x4 = new Item("cheese4", 3, eType.food, eCosher.milk, d4, 2.5);
+            Shelf y2 = new Shelf(4, 25);
+            y2.ItemsList.Add(x3);
+            y2.ItemsList.Add(x4);
+
+            Refrigerator r = new Refrigerator("Samsung", "gray", 2);
+            r.ShelvesList.Add(y);
+            r.ShelvesList.Add(y2);
+
+            Shelf y3 = new Shelf(4, 15);
+            Shelf y4=new Shelf(5, 10);
+
+            Refrigerator r2 = new Refrigerator("LG", "White", 3);
+            r2.ShelvesList.Add(y);
+            r2.ShelvesList.Add(y3);
+            r2.ShelvesList.Add(y4);
+
+            Console.WriteLine("Press 1: Print all the items on the refrigerator and all its contents.\r\nPress 2: Print how much space is left in the refrigerator\r\nPress 3: Add an item to the refrigerator .\r\nPress 4: Remove an item from the refrigerator.\r\nPress 5: Clean the refrigerator and print all the checked items.\r\nPress 6: What do I want to eat? \r\nPress 7: Print all the products sorted by their expiration date.\r\nPress 8: Print all the shelves arranged according to the free space left on them.\r\nPress 9: Print all the refrigerators arranged according to the free space left in them.\r\nPress 10: Prepare the refrigerator for shopping\r\nPress 100: system shutdown.");
+            int UserChoose;
+            
+            
+            do
+            {
+                Console.WriteLine("CHOOSE COMMAND NUMBER!");
+                UserChoose = int.Parse(Console.ReadLine());
+                switch (UserChoose)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine(r.ToString());
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine(r.PlaceLeft());
+                            break;
+                        }
+                    case 3:
+                        {
+
+                            r.AddItem();
+                            break;
+
+                        }
+                    case 4:
+                        {
+                            Console.WriteLine("Enter the item Id!");
+                            int ItemId = int.Parse(Console.ReadLine());
+                            Console.WriteLine(r.TakeItemOut(ItemId));
+                            break;
+                        }
+                    case 5:
+                        {
+                            r.DeleteExpired();
+                            Console.WriteLine(r.ToString());
+                            break;
+
+                        }
+                    case 6:
+                        {
+                            Console.WriteLine("WHat should I eat?  Enter type anf cosher!");
+                            eCosher cosher = 0;
+                            eType type = 0;
+                            Refrigerator.StringToEnumParser(ref type, ref cosher);
+                            
+                            if(r.WhatShouldIEat(type, cosher)!=null)
+                                Console.WriteLine(r.WhatShouldIEat(type, cosher));
+                            break;
+                        }
+                    case 7:
+                        {
+                            (r.SortByExpiryDate()).ForEach(item => Console.WriteLine(item));
+                            break;
+
+                        }
+
+                    case 8:
+                        {
+
+                            (r.SortShelfByPlaceLeft()).ForEach(item => Console.WriteLine(item));
+                            break;
+
+                        }
+                    case 9:
+                        {
+                            Refrigerator.SortFrigeByPlaceLeft().ForEach(item => Console.WriteLine(item));
+                            break;
+
+
+                        }
+                    case 10:
+                        {
+                            r.Shopping();
+                            break;
+                        }
+                    case 100:
+                        {
+                            Console.WriteLine("The system is turned OFF!");
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("ERROR:no Command found!");
+                            break;
+                        }
+
+                }
+            }
+            while (UserChoose != 100);
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ TheRefrigerator/Refrigerator.cs
@@ -0,0 +1,332 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using TheRefrigerator;
+
+namespace TheRefrigerator
+{
+    public class Refrigerator
+    {
+        //A counter to ensure an unique ID
+        public static int Counter = 1;
+        public int Id { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int NumOfShells { get; set; }
+        public List<Shelf> ShelvesList { get; set; }
+        public static List<Refrigerator> All = new List<Refrigerator>();
+
+        public Refrigerator(string model, string color, int numOfShells)
+        {
+            Id = Counter++;
+            Model = model;
+            Color = color;
+            NumOfShells = numOfShells;
+            ShelvesList = new List<Shelf>();
+            All.Add(this);
+
+
+        }
+
+        public override string ToString()
+        {
+            string x = "";
+            x += Color + " Refrigerator, model " + Model + " with " + NumOfShells + " shelves: " + Environment.NewLine;
+            foreach (Shelf sh in ShelvesList)
+            {
+                x += sh.ToString();
+                x += "  ";
+
+            }
+            return x;
+        }
+
+        public double PlaceLeft()
+        {
+            double sum = 0;
+            foreach (Shelf sh in ShelvesList)
+            {
+                sum += sh.PlaceLeft();
+            }
+            return sum;
+        }
+
+        public void AddItem()
+        {
+            Console.WriteLine("Now we are going to add an item to the refrigerator!" + Environment.NewLine
+                + " Please enter details: Name, Type, Cosher, ExpiryDate And Size (In cm)");
+
+            string Name = Console.ReadLine();
+            int shelfNum = 0;
+
+
+            eType etype = 0;
+            eCosher eCosher = 0;
+            StringToEnumParser(ref etype, ref eCosher);
+
+            DateTime date = DateTime.Parse(Console.ReadLine());
+            double Size = double.Parse(Console.ReadLine());
+            Item NewItem = null;
+            foreach (Shelf sh in ShelvesList)
+            {
+                if (sh.PlaceLeft() >= Size)
+                {
+                    shelfNum = sh.Id;
+                    NewItem = new Item(Name, shelfNum, etype, eCosher, date, Size);
+                    break;
+                }
+
+            }
+            if (shelfNum == 0)
+            {
+                Console.WriteLine("ERROR:Not enough place!");
+                return;
+            }
+
+
+            //Console.WriteLine("----------- " + NewItem);
+
+            Shelf chosenShelf = ShelvesList.Find(x => x.Id == shelfNum);
+            chosenShelf.ItemsList.Add(NewItem);
+            Console.WriteLine(chosenShelf);
+
+
+        }
+
+        public static void StringToEnumParser(ref eType etype, ref eCosher ecosher)
+        {
+            bool IsValidType = false;
+            string type;
+            do
+            {
+                Console.WriteLine("You can only choose food  or drink!");
+                type = Console.ReadLine();
+                type.ToLower();
+                IsValidType = Enum.TryParse(type, out eType hlp);
+            }
+
+            while (!IsValidType);
+
+            etype = (eType)Enum.Parse(typeof(eType), type);
+
+
+            bool IsValidCosher = false;
+            string cosher;
+            do
+            {
+                Console.WriteLine("You can only choose milk, meat or parve!");
+                cosher = Console.ReadLine();
+                cosher.ToLower();
+                IsValidCosher = Enum.TryParse(cosher, out eCosher hlp);
+            }
+
+            while (!IsValidCosher);
+
+            ecosher = (eCosher)Enum.Parse(typeof(eCosher), cosher);
+
+
+        }
+
+        public Item TakeItemOut(int Id)
+        {
+            foreach (Shelf item in ShelvesList)
+            {
+                Item toDelete = item.ItemsList.Find(x => x.Id == Id);
+                if (toDelete != null)
+                {
+                    item.ItemsList.Remove(toDelete);
+                    return toDelete;
+
+                }
+
+            }
+            Console.WriteLine("Item not found!");
+            return null;
+
+        }
+
+        public void DeleteExpired()
+        {
+            List<Item> ToDelete, AllDeletedItems = new List<Item>();
+
+            int i = 0;
+            foreach (Shelf item in ShelvesList)
+            {
+                ToDelete = item.ItemsList.Where(x => x.ExpiryDate < DateTime.Now).ToList();
+                AllDeletedItems.Add(ToDelete[0]);
+                item.ItemsList.Remove(ToDelete[0]);
+            }
+            Console.WriteLine("All deleted items");
+            AllDeletedItems.ForEach(item => Console.WriteLine(item));
+            Console.WriteLine("Delete Expired Done!");
+        }
+
+        public Item WhatShouldIEat(eType type, eCosher cosher)
+        {
+
+            foreach (Shelf item in ShelvesList)
+            {
+                List<Item> ToEat = item.ItemsList.Where(x => x.Cosher == cosher && x.Type == type && x.ExpiryDate >= DateTime.Now).ToList();
+
+                if (ToEat.Count != 0)
+                    return ToEat[0];
+
+            }
+            Console.WriteLine("There is no match item!");
+            return null;
+        }
+
+
+
+        public List<Item> SortByExpiryDate()
+        {
+
+            List<Item> allItems = new List<Item>();
+            foreach (Shelf item in ShelvesList)
+            {
+                foreach (Item item2 in item.ItemsList)
+                {
+                    allItems.Add(item2);
+                }
+            }
+
+            List<Item> sortItems = allItems.OrderBy(item => item.ExpiryDate).ToList();
+
+
+
+            return sortItems;
+        }
+
+
+
+
+        public List<Shelf> SortShelfByPlaceLeft()
+        {
+
+            List<Shelf> sortShelves = ShelvesList.OrderByDescending(item => item.PlaceLeft()).ToList();
+            return sortShelves;
+
+        }
+
+        public static List<Refrigerator> SortFrigeByPlaceLeft()
+        {
+            List<Refrigerator> sortFridges = All.OrderByDescending(item => item.PlaceLeft()).ToList();
+            return sortFridges;
+
+
+        }
+
+        public void Shopping()
+        {
+            double sum = 0;
+            if (CheckIfCanShopping(this.PlaceLeft()))
+                return;
+
+
+            else
+            {
+                this.DeleteExpired();
+
+                if (CheckIfCanShopping(this.PlaceLeft()))
+                    return;
+
+                List<int> milksId = new List<int>(),
+                    meatsId = new List<int>(),
+                    parvesId = new List<int>();
+
+
+                foreach (Shelf shelf in ShelvesList)
+                {
+
+                    foreach (Item item in shelf.ItemsList)
+                    {
+                        if (item.Cosher == eCosher.milk && item.ExpiryDate.AddDays(-3) <= DateTime.Now)
+                        {
+                            
+                            milksId.Add(item.Id);
+                            sum += item.Size;
+                        }
+
+                        if (item.Cosher == eCosher.meat && item.ExpiryDate.AddDays(-7) <= DateTime.Now)
+                        {
+                            meatsId.Add(item.Id);
+                            sum += item.Size;
+                        }
+
+                        if (item.Cosher == eCosher.parve && item.ExpiryDate.AddDays(-1) <= DateTime.Now)
+                        {
+                            parvesId.Add(item.Id);
+                            sum += item.Size;
+                        }
+
+                    }
+
+
+                }
+                if (CheckIfCanShopping(sum))
+                    return;
+
+                DeleteForShopping(milksId);
+
+                if (CheckIfCanShopping(sum))
+                    return;
+
+                DeleteForShopping(meatsId);
+
+                if (CheckIfCanShopping(sum))
+                    return;
+
+                DeleteForShopping(meatsId);
+                return;
+
+
+
+
+
+            }
+
+        }
+
+        public bool CheckIfCanShopping(double sum)
+        {
+            if (sum >= 20)
+                Console.WriteLine("You can go shopping!");
+
+            return sum >= 20;
+        }
+
+        public void DeleteForShopping(List<int> ItemsId)
+        {
+            int length;
+           
+            foreach (Shelf shelf in ShelvesList)
+            {
+                length = shelf.ItemsList.Count;
+                for (int i = 0; i < length; i++)
+                {
+
+                    if (i<ItemsId.Count && shelf.ItemsList.Count>0&& ItemsId.Contains(shelf.ItemsList[i].Id))
+                    {
+                        Console.WriteLine("Deleted: " + shelf.ItemsList[i].Name);
+                        shelf.ItemsList.Remove(shelf.ItemsList[i]);
+                        i--;
+
+                    }
+                }
+
+
+            }
+        }
+
+
+
+
+
+
+    }
+}
